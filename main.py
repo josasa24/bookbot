078d7bfb0aba63c5d662d21e6542ed7077bb9ee5
josasa24 <samusanders@gmail.com>
2025-03-30 21:40:08 -0500

    Updating main.py for words

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        return f.read()
+
+def main(book_text):
+    print(book_text)
+
+# Call the functions
+book_text = get_book_text("books/frankenstein.txt")
+main(book_text)
+
+def number_of_words(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        return f.read()
